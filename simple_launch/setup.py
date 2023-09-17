638e825ec60c20d82a3c2a0dd70886f5018be0dd
Hem92j <rauljihemraj@gmail.com>
2023-09-17 17:06:19 +0200

    updated sensor_publisher.py

---
+++ simple_launch/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'simple_launch'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='hem',
+    maintainer_email='rauljihemraj@gmail.com',
+    description='This repo will go through different concepts of launch files in ROS 2 (Humble Hawksbill).',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "sensor_data_pub = simple_launch.sensor_publisher:main"
+        ],
+    },
+)
---
+++ simple_launch/simple_launch/sensor_publisher.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SensorPublisher(Node):
+    def __init__(self):
+        super().__init__("sensor_publisher")
+        self.sensor_pub_ = self.create_publisher(String, "data_publisher", 10)
+        self.sensor_timer =   self.create_timer(1.0, self.sensor_timer_callback)
+
+    def sensor_timer_callback(self):
+        msg = String()
+        msg.data = "I am publishing sensor data in String format..."
+        self.sensor_pub_.publish(msg)
+
+
+def main(args= None):
+    rclpy.init(args=args)
+    node = SensorPublisher()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
